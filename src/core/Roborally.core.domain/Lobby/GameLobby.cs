434ba36d0d953365e8960e9378c24ada8ca77673
MrSachin7 <nepalghi@gmail.com>
2025-09-16 16:20:17 +0200

    Copilot issues

--- src/core/Roborally.core.domain/Lobby/GameLobby.cs
+++ src/core/Roborally.core.domain/Lobby/GameLobby.cs
@@ -3,14 +3,15 @@
 namespace Roborally.core.domain.Lobby;
 
 public class GameLobby {
-    public List<User.User> JoinedUsers { get; init; }
+    private const int MaxLobbySize = 6;
+    private List<User.User> JoinedUsers { get; init; }
     public required Guid GameId { get; init; }
     public required bool IsPrivate { get; set; }
     public Guid HostId { get; init; }
 
     public GameLobby(User.User hostUser, bool isPrivate) {
         HostId = hostUser.Id;
-        JoinedUsers = new List<User.User>(6) {
+        JoinedUsers = new List<User.User>(MaxLobbySize) {
             // Host already enters the lobby
             hostUser
         };
@@ -19,7 +20,7 @@ public class GameLobby {
     }
 
     public void JoinLobby(User.User user) {
-        if (JoinedUsers.Count >= 6) {
+        if (JoinedUsers.Count >= MaxLobbySize) {
             throw new CustomException("Lobby is full", 400);
         }
 
@@ -28,5 +29,9 @@ public class GameLobby {
         }
 
         JoinedUsers.Add(user);
+    }
+
+    public IReadOnlyList<User.User> GetUsersInLobby() {
+        return JoinedUsers.AsReadOnly();
     }
 }
