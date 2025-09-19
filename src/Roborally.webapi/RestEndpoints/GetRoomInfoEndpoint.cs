3219f37a5c0375f6ef373f014d5f2b34e215c7f0
MrSachin7 <nepalghi@gmail.com>
2025-09-19 16:33:16 +0200

    Save chanages

---
+++ src/Roborally.webapi/RestEndpoints/GetRoomInfoEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using FastEndpoints;
+using Roborally.core.application.CommandContracts;
+
+namespace Roborally.webapi.RestEndpoints;
+
+public class GetRoomInfoEndpoint : Endpoint<GetRoomInfoRequest, GetRoomInfoResponse> {
+    public override void Configure() {
+        Get("/game-lobbies/{GameId}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(GetRoomInfoRequest req, CancellationToken ct) {
+        GetRoomInfoCommand command = new GetRoomInfoCommand() {
+            GameId = req.GameId,
+            Username = req.Username
+        };
+    }
+}
+
+public class GetRoomInfoRequest {
+    [FromHeader] public string Username { get; set; }
+    public Guid GameId { get; set; }
+}
+
+public class GetRoomInfoResponse {
+    public Guid GameId { get; set; }
+    public List<string> ConnectedUsernames { get; set; }
+}
---
+++ src/core/Roborally.core.application/CommandContracts/GetRoomInfoCommand.cs
@@ -0,0 +1,8 @@
+﻿using FastEndpoints;
+
+namespace Roborally.core.application.CommandContracts;
+
+public class GetRoomInfoCommand : ICommand<string> {
+    public string Username { get; set; }
+    public Guid GameId { get; set; }
+}                                                              
