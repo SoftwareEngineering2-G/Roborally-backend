c020ecb34d602aed0154dfe1a00d72effefcd2d1
Andrej Jurco <jurco10510@gmail.com>
2025-10-01 09:31:30 +0200

    get game endpoint and rep. edit

---
+++ src/core/Roborally.core.application/CommandHandlers/GetGameCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using FastEndpoints;
+using Roborally.core.application.CommandContracts;
+using Roborally.core.domain.Game;
+
+namespace Roborally.core.application.CommandHandlers;
+
+public class GetGameCommandHandler:ICommandHandler<GetGameCommand,GetGameCommandResponse>
+{
+    private readonly IGameRepository _gameRepository;
+
+    public GetGameCommandHandler(IGameRepository gameRepository)
+    {
+        _gameRepository = gameRepository;
+    }
+
+    public async Task<GetGameCommandResponse> ExecuteAsync(GetGameCommand command, CancellationToken ct)
+    {
+        Game? game = await _gameRepository.FindAsync(command.GameId,ct);
+        if (game is null)
+        {
+            throw new CustomException("Game not found", 404);
+        }
+
+        return new GetGameCommandResponse()
+        {
+            Game = game
+        };
+    }
+}
