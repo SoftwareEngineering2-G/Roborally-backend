1790c8ab747c639ce8e28b348a3c3a1697a64464
VicAl02 <vincenzo.altaserse@gmail.com>
2025-10-12 19:34:20 +0200

    Add unit tests for ActivateBoardElementCommandHandler and related classes

---
+++ tests/Roborally.unitTests/Factory/DirectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using Roborally.core.domain.Game.Player;
+
+namespace Roborally.unitTests.Factory;
+
+public class DirectionFactory
+{
+    public static IEnumerable<object[]> GetValidDirections()
+    {
+        return new List<object[]>()
+        {
+            new object[] { Direction.North },
+            new object[] { Direction.South },
+            new object[] { Direction.East },
+            new object[] { Direction.West },
+        };
+    }
+}
