b9a96b578c3e344574745f2892bf6e00ed1ab908
Andrej Jurco <61362831+hanchry@users.noreply.github.com>
2025-11-14 10:12:24 +0100

    Game termination (#46)

---
+++ src/Roborally.webapi/RestEndpoints/Game/Commands/EndGameEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using FastEndpoints;
+using Roborally.core.application.CommandContracts;
+
+namespace Roborally.webapi.RestEndpoints.Lobby;
+
+public class EndGameEndpoint: Endpoint<EndGameRequest>
+{
+    public override void Configure()
+    {
+        Post("/games/{gameId}/end");
+    }
+    
+    public override async Task HandleAsync(EndGameRequest req, CancellationToken ct)
+    {
+        EndGameCommand command = new EndGameCommand()
+        {
+            GameId = req.GameId,
+            Username = req.WinnerUsername
+        };
+        
+        await command.ExecuteAsync(ct);
+
+        await Send.OkAsync(cancellation: ct);
+    }
+    
+}
+
+public class EndGameRequest
+{
+    public Guid GameId { get; set; }
+    public string WinnerUsername { get; set; }
+}
---
+++ src/core/Roborally.core.application/CommandContracts/Game/EndGameCommand.cs
@@ -0,0 +1,9 @@
+﻿using FastEndpoints;
+
+namespace Roborally.core.application.CommandContracts;
+
+public class EndGameCommand:ICommand
+{
+    public required Guid GameId { get; init; }
+    public required string Username { get; init; }
+}
